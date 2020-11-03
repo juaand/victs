78e7730ef3a76e0df9599bed91b5d98623827859
Andres Martinez <andresmartgonz@gmail.com>
2020-11-03 16:52:58 +0100

    routes check

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -16,28 +16,31 @@ http.interceptors.response.use(function (response) {
   return Promise.reject(error)
 })
 
-export const getGymDetail = (id) => http.get(`/gym-detail/${id}`)
 
+//gym.routes
+export const getGyms = () => http.get('/gyms')
+export const getGymDetail = (id) => http.get(`/gym-detail/${id}`)
 export const getGymClassrooms = (id) => http.get(`/gym-classrooms/${id}`)
 
-export const getGyms = () => http.get('/gyms')
-
+//instructor.routes
 export const getInstructors = () => http.get(`/instructors`)
 
-export const getDisciplines = () => http.get('/disciplines')
+//user.routes
+export const login = ({email, password}) => http.post('/login', {email, password})
+export const logOut = () => http.post('/logout')
+export const activateUser = (token) => http.get(`/activate/${token}`)
+export const stripeInfo = ({data, id, plan}) => http.post('/stripe/checkout', {id, plan, data})
+export const followInfo = (id) => http.post(`/follow/${id}`, {id})
+export const booking = (id, row, seat) => http.post(`/book/${id}`, {id, row, seat})
+export const unbooking = (reservations) => http.post(`/unbook`, {reservations})
 
+//data.routes
+export const getDisciplines = () => http.get('/disciplines')
 export const getServices = () => http.get('/services')
 
-export const login = ({email, password}) => http.post('/login', {email, password})
-
-export const logOut = () => http.post('/logout')
-
+//crud.routes
 export const register = ({name, email, password, role}) => http.post('/register', {name, email, password, role})
-
-export const activateUser = (token) => http.get(`/activate/${token}`)
-
 export const updateUser = ({name, role, address, phone, city, zipcode, id, quote, services, disciplines}) => http.patch(`/user-profile/${id}/edit`, {name, role, address, phone, city, zipcode, id, quote, services, disciplines})
-
 export const updateUserAvatar = (data, id) => {
   let fd = new FormData()
   fd.append('file', data)
@@ -45,25 +48,18 @@ export const updateUserAvatar = (data, id) => {
   const config = {headers: {'Content-Type': 'multipart/form-data'}}
   return http.post(`/user-profile/${id}/edit-avatar`, fd, config)
 }
-
 export const updatePassword = ({password, newpassword, id}) => http.post(`/update-password/${id}`, {password, newpassword})
-
 export const deleteUser = (id) => http.get(`/user/${id}/delete`)
 
-export const stripeInfo = ({data, id, plan}) => http.post('/stripe/checkout', {id, plan, data})
 
-export const followInfo = (id) => http.post(`/follow/${id}`, {id})
 
+
+
+
+
+
+//lesson.routes
+export const createLesson = () => http.post('/lesson')
 export const lessonInfo = (id) => http.get(`/lesson/${id}`, {id})
-
-export const booking = (id, row, seat) => http.post(`/book/${id}`, {id, row, seat})
-
-export const unbooking = (reservations) => http.post(`/unbook`, {reservations})
-
-export const createLesson = () => http.post('/lesson') 
-
-export const readLesson = (id) => http.post(`/lesson/${id}`)
-
 export const updateLesson = (id) => http.post(`/lesson/${id}/edit`)
-
 export const deleteLesson = (id) => http.post(`/lesson/${id}/delete`)
