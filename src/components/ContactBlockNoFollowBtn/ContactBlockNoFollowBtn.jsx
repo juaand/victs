38eb0501b50037f88a2da5e5447b5124a75f54a4
Andres Martinez <andresmartgonz@gmail.com>
2020-11-05 16:39:59 +0100

    rct029

--- src/components/ContactBlockNoFollowBtn/ContactBlockNoFollowBtn.jsx
+++ src/components/ContactBlockNoFollowBtn/ContactBlockNoFollowBtn.jsx
@@ -1,20 +1,48 @@
 import './ContactBlockNoFollowBtn.css'
-import React from 'react'
+import React, {useState} from 'react'
+import {getFollowersUsers} from '../../services/ApiClient'
 
 const ContactBlockNoFollowBtn = ({contactInfo}) => {
 
+    const followersArr = contactInfo.user.followers
+
+    const [bool, setBool] = useState(false)
+    const [followersList, setFollowersList] = useState([])
+
+    const showFollowers = async () => {
+        setBool(!bool)
+        const res = await getFollowersUsers(followersArr)
+        setFollowersList(res)
+        console.log(res)
+    }
+
+    const hideModal = () => {
+        setBool(!bool)
+    }
+
     return (
-        <div className="container gym-info">
-            <div className="row">
-                <div className="col-4">
-                    <span className="address">{contactInfo.user.address}</span>
-                    <span className="contact">
-                        <strong>P</strong> {contactInfo.user.phone}<br />
-                        <strong>M</strong> {contactInfo.user.email}
-                    </span>
+        <>
+            {bool &&
+                <section className="followers-modal">
+                <span className="close" onClick={hideModal}>close</span>
+                {followersList.map(el => <p>{el.name}</p>)}
+                </section>
+            }
+            <div className="container gym-info">
+                <div className="row">
+                    <div className="col-4">
+                        <span className="address">{contactInfo.user.address}</span>
+                        <span className="contact">
+                            <strong>P</strong> {contactInfo.user.phone}<br />
+                            <strong>M</strong> {contactInfo.user.email}
+                            <div className="followers" onClick={showFollowers}>
+                                <strong>F</strong> {contactInfo.user.followers.length} followers
+                        </div>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from "axios"
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
-});
+})
 
 http.interceptors.response.use(
   function (response) {
-    return response.data;
+    return response.data
   },
   function (error) {
     if (error.response?.status === 401) {
-      localStorage.clear();
-      window.location.assign("/login");
+      localStorage.clear()
+      window.location.assign("/login")
     }
 
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 //gym.routes
 export const getGyms = () => http.get('/gyms')
@@ -27,28 +27,29 @@ export const getGymLessons = (id) => http.get(`/gym-lessons/${id}`)
 
 
 //instructor.routes
-export const getInstructors = () => http.get(`/instructors`);
+export const getInstructors = () => http.get(`/instructors`)
 
 //user.routes
-export const login = ({ email, password }) =>
-  http.post("/login", { email, password });
-export const logOut = () => http.post("/logout");
-export const activateUser = (token) => http.get(`/activate/${token}`);
-export const stripeInfo = ({ data, id, plan }) =>
-  http.post("/stripe/checkout", { id, plan, data });
-export const followInfo = (id) => http.post(`/follow/${id}`, { id });
+export const login = ({email, password}) =>
+  http.post("/login", {email, password})
+export const logOut = () => http.post("/logout")
+export const activateUser = (token) => http.get(`/activate/${token}`)
+export const stripeInfo = ({data, id, plan}) =>
+  http.post("/stripe/checkout", {id, plan, data})
+export const followInfo = (id) => http.post(`/follow/${id}`, {id})
 export const booking = (id, row, seat) =>
-  http.post(`/book/${id}`, { id, row, seat });
+  http.post(`/book/${id}`, {id, row, seat})
 export const unbooking = (reservations) =>
-  http.post(`/unbook`, { reservations });
+  http.post(`/unbook`, {reservations})
+export const getFollowersUsers = (followers) => http.post('/followers-users', followers)
 
 //data.routes
-export const getDisciplines = () => http.get("/disciplines");
-export const getServices = () => http.get("/services");
+export const getDisciplines = () => http.get("/disciplines")
+export const getServices = () => http.get("/services")
 
 //crud.routes
-export const register = ({ name, email, password, role }) =>
-  http.post("/register", { name, email, password, role });
+export const register = ({name, email, password, role}) =>
+  http.post("/register", {name, email, password, role})
 export const updateUser = ({
   name,
   role,
@@ -72,17 +73,17 @@ export const updateUser = ({
     quote,
     services,
     disciplines,
-  });
+  })
 export const updateUserAvatar = (data, id) => {
-  let fd = new FormData();
-  fd.append("file", data);
+  let fd = new FormData()
+  fd.append("file", data)
 
-  const config = { headers: { "Content-Type": "multipart/form-data" } };
-  return http.post(`/user-profile/${id}/edit-avatar`, fd, config);
-};
-export const updatePassword = ({ password, newpassword, id }) =>
-  http.post(`/update-password/${id}`, { password, newpassword });
-export const deleteUser = (id) => http.get(`/user/${id}/delete`);
+  const config = {headers: {"Content-Type": "multipart/form-data"}}
+  return http.post(`/user-profile/${id}/edit-avatar`, fd, config)
+}
+export const updatePassword = ({password, newpassword, id}) =>
+  http.post(`/update-password/${id}`, {password, newpassword})
+export const deleteUser = (id) => http.get(`/user/${id}/delete`)
 
 //lesson.routes
 export const createLesson = ({
@@ -112,8 +113,8 @@ export const createLesson = ({
     instructor,
     gym,
     classroom
-  });
-export const lessonInfo = (id) => http.get(`/lesson/${id}`, { id });
+  })
+export const lessonInfo = (id) => http.get(`/lesson/${id}`, {id})
 export const updateLesson = ({
   name,
   address,
@@ -143,25 +144,25 @@ export const updateLesson = ({
     id,
     classroom,
     gym
-  });
+  })
 
-export const deleteLesson = (id) => http.get(`/lesson/${id}/delete`);
-export const getLessonGuests = (id) => http.get(`/lesson-guests/${id}`);
+export const deleteLesson = (id) => http.get(`/lesson/${id}/delete`)
+export const getLessonGuests = (id) => http.get(`/lesson-guests/${id}`)
 
 //classroom routes
 
-export const createClassroom = ({ user, gym, name, rows, discipline }) =>
+export const createClassroom = ({user, gym, name, rows, discipline}) =>
   http.post("/classroom", {
     user,
     gym,
     name,
     rows,
     discipline,
-  });
+  })
 
-export const classroomInfo = (id) => http.get(`/classroom/${id}`, { id });
+export const classroomInfo = (id) => http.get(`/classroom/${id}`, {id})
 
-export const updateClassroom = ({ user, gym, name, rows, discipline, id }) =>
+export const updateClassroom = ({user, gym, name, rows, discipline, id}) =>
   http.patch(`/classroom/${id}/edit`, {
     user,
     gym,
@@ -169,8 +170,8 @@ export const updateClassroom = ({ user, gym, name, rows, discipline, id }) =>
     rows,
     discipline,
     id,
-  });
+  })
 
-export const deleteClassroom = (id) => http.get(`/classroom/${id}/delete`);
+export const deleteClassroom = (id) => http.get(`/classroom/${id}/delete`)
 
 export const getClassroomLessons = (id) => http.get(`/classroom-lessons/${id}`)
