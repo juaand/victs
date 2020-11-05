930bc4b25867b3c32dbc92d35eec0f29402338dd
Andres Martinez <andresmartgonz@gmail.com>
2020-11-05 19:10:39 +0100

    followers

--- src/components/ContactBlockNoFollowBtn/ContactBlockNoFollowBtn.jsx
+++ src/components/ContactBlockNoFollowBtn/ContactBlockNoFollowBtn.jsx
@@ -1,48 +1,22 @@
 import './ContactBlockNoFollowBtn.css'
-import React, {useState} from 'react'
-import {getFollowersUsers} from '../../services/ApiClient'
+import React from 'react'
 
-const ContactBlockNoFollowBtn = ({contactInfo}) => {
-
-    const followersArr = contactInfo.user.followers
-
-    const [bool, setBool] = useState(false)
-    const [followersList, setFollowersList] = useState([])
-
-    const showFollowers = async () => {
-        setBool(!bool)
-        const res = await getFollowersUsers(followersArr)
-        setFollowersList(res)
-        console.log(res)
-    }
-
-    const hideModal = () => {
-        setBool(!bool)
-    }
-
+const ContactBlockNoFollowBtn = ({contactInfo, onClick}) => {
     return (
-        <>
-            {bool &&
-                <section className="followers-modal">
-                <span className="close" onClick={hideModal}>close</span>
-                {followersList.map(el => <p>{el.name}</p>)}
-                </section>
-            }
-            <div className="container gym-info">
-                <div className="row">
-                    <div className="col-4">
-                        <span className="address">{contactInfo.user.address}</span>
-                        <span className="contact">
-                            <strong>P</strong> {contactInfo.user.phone}<br />
-                            <strong>M</strong> {contactInfo.user.email}
-                            <div className="followers" onClick={showFollowers}>
-                                <strong>F</strong> {contactInfo.user.followers.length} followers
+        <div className="container gym-info">
+            <div className="row">
+                <div className="col-4">
+                    <span className="address">{contactInfo.user.address}</span>
+                    <span className="contact">
+                        <strong>P</strong> {contactInfo.user.phone}<br />
+                        <strong>M</strong> {contactInfo.user.email}
+                        <div className="followers" onClick={onClick}>
+                            <strong>F</strong> {contactInfo.user.followers.length} followers
                         </div>
-                        </span>
-                    </div>
+                    </span>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/components/MyInfo/MyInfoGym/MyInfoGym.jsx
+++ src/components/MyInfo/MyInfoGym/MyInfoGym.jsx
@@ -1,7 +1,7 @@
 import './MyInfoGym.css'
 import React, {useState, useEffect} from 'react'
 import UserAccordeon from '../../UserAccordeon/UserAccordeon'
-import {getGymLessons, getGymClassrooms} from '../../../services/ApiClient'
+import {getGymLessons, getGymClassrooms, getFollowersUsers} from '../../../services/ApiClient'
 import ContactBlockNoFollowBtn from '../../ContactBlockNoFollowBtn/ContactBlockNoFollowBtn'
 import ContentWithLessons from '../../ContentWithLessons/ContentWithLessons'
 import ContentWithInstructors from '../../ContentWithInstructors/ContentWithInstructors'
@@ -13,6 +13,8 @@ const MyInfoGym = ({user, gym}) => {
     const [userStatus, setUserStatus] = useState(user)
     const [gymLessons, setGymLessons] = useState([])
     const [gymClassrooms, setGymClassrooms] = useState([])
+    const [followersList, setFollowersList] = useState([])
+    const [bool, setBool] = useState(false)
     const history = useHistory()
 
     const editLesson = async (lesson) => {
@@ -20,6 +22,17 @@ const MyInfoGym = ({user, gym}) => {
             pathname: '/edit-lesson',
             state: {lesson: lesson}
         })
+    }
+
+    const showFollowers = async () => {
+        setBool(!bool)
+        const res = await getFollowersUsers(gym.user.followers)
+        setFollowersList(res)
+        console.log(res)
+    }
+
+    const hideModal = () => {
+        setBool(!bool)
     }
 
     useEffect(() => {
@@ -45,8 +58,26 @@ const MyInfoGym = ({user, gym}) => {
 
     return (
         <section className="MyInfoGym">
+            {bool &&
+                <section className="followers-modal modal">
+                    <div className="container">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-sm-6 col-12 followers-content modal-body">
+                                <span className="close" onClick={hideModal}></span>
+                                <h1>{gym.user.name} <br />followers</h1>
+                                {followersList.map(el =>
+                                    <div className="follower">
+                                        <span className="avatar" style={{background: `url(${el.avatar}) no-repeat center center / cover`}}>
+                                        </span>
+                                        {el.name}
+                                    </div>)}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            }
             <UserAccordeon user={userStatus} gymInfo={gym} />
-            <ContactBlockNoFollowBtn contactInfo={gym} />
+            <ContactBlockNoFollowBtn contactInfo={gym} onClick={showFollowers} />
             {gymLessons ? <ContentWithLessons onClick={editLesson} title="Upcoming lessons" data={gymLessons} /> : 'Loading'}
             {gymLessons ? <ContentWithInstructors title="Instructors" data={gymLessons} /> : 'Loading'}
             {gymClassrooms ? <ContentWithClassrooms title="Classrooms" data={gymClassrooms} /> : 'Loading'}
