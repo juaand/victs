ea591e2c72c4724bebad1eb9fa53c7ad55df65ef
Andres Martinez <andresmartgonz@gmail.com>
2020-11-18 16:16:58 +0100

    hs019

--- src/components/Layouts/Calendar/Calendar.jsx
+++ src/components/Layouts/Calendar/Calendar.jsx
@@ -8,23 +8,25 @@ import {getAllLessons} from '../../../services/ApiClient'
 export default function Calendar({user}) {
 
     const colArr = []
-    const today = new Date().getDate()
+    const today = new Date()
     const [currentDay, setCurrentDay] = useState(new Date())
     const [lessons, setLessons] = useState(user?.lessons)
     const [bool, setBool] = useState(false)
 
     const drawColumns = (date) => {
-        const number = date.getDate()
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        const currentMonth = months[date.getMonth()]
 
-        for (let i = number; i < number + 7; i++) {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+        for (let i = 0; i < 7; i++) {
+            let next = new Date(date)
+            next.setDate(date.getDate() + i)
             colArr.push(
                 <div className="col box">
-                    <div className={i === number ? 'date-num current' : 'date-num'}>{i}
-                        <span className="month">{currentMonth}</span>
+                    <div className={(date.getDate() + i) === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear() ? 'date-num current' : 'date-num'}>
+                        {new Date(next).getDate()}
+                        <span className="month">{months[new Date(next).getMonth()]}</span>
                     </div>
-                    {lessons.filter(el => new Date(el.date).getDate() === i && new Date(el.date).getMonth() === currentDay.getMonth() && new Date(el.date).getFullYear() === currentDay.getFullYear()).map(el => <CalendarItem data={el} />)}
+                    {lessons.filter(el => new Date(el.date).getDate() === date.getDate() + i && new Date(el.date).getMonth() === currentDay.getMonth() && new Date(el.date).getFullYear() === currentDay.getFullYear()).map(el => <CalendarItem data={el} />)}
                 </div>)
         }
 
@@ -34,7 +36,6 @@ export default function Calendar({user}) {
     const getLessons = async () => {
         const allLessons = await getAllLessons()
         setLessons(allLessons)
-        console.log(allLessons)
     }
 
     const changeLessonsView = (e) => {
@@ -50,9 +51,14 @@ export default function Calendar({user}) {
     const prevWeek = () => {
         setCurrentDay(new Date(currentDay.getFullYear(), currentDay.getMonth(), currentDay.getDate() - 7))
         drawColumns(currentDay)
-        if (today === currentDay.getDate() - 7) {
+        if (today.getDate() === currentDay.getDate() - 7) {
             setBool(!bool)
         }
+    }
+
+    const setToday = () => {
+        setCurrentDay(today)
+        setBool(false)
     }
 
     return (
@@ -65,7 +71,7 @@ export default function Calendar({user}) {
             </div>
             <div className="row week-select">
                 <div className="col-12 btns-row">
-                    <span className="today">TODAY</span>
+                    <span className="today" onClick={setToday}>TODAY</span>
                     <span className={bool ? 'prev' : 'prev disabled'} onClick={prevWeek}></span>
                     <span className="next" onClick={nextWeek}></span>
                 </div>
