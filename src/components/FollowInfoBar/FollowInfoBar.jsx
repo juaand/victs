7cc83dd04fc87612eeb336240278fb8bd5ebc2e3
Andres Martinez <andresmartgonz@gmail.com>
2020-11-27 12:02:53 +0100

    rct000

--- src/components/FollowInfoBar/FollowInfoBar.jsx
+++ src/components/FollowInfoBar/FollowInfoBar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function FollowInfoBar({followInfo, onClick}) {
     return (
-        <div className={followInfo.following.length > 0 ? 'followers disabled' : 'followers'} onClick={onClick}>
+        <div className={followInfo.followers.length > 0 ? 'followers disabled' : 'followers'} onClick={onClick}>
             {followInfo.followers.length} followers
         </div>
     )
